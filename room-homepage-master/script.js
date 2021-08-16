2a07225cfb1a05e6218ed2e993daafb92acc4436
Renato Marques <67842526+renatomarquesco@users.noreply.github.com>
2021-08-16 21:03:49 +0200

    Add files via upload
    first commit

---
+++ room-homepage-master/script.js
@@ -0,0 +1,34 @@
+let margin = 0;
+let wrapper = document.getElementById("wrapper");
+let leftAngle = document.querySelectorAll(".left-angle")
+let rightAngle = document.querySelectorAll(".right-angle")
+let showNavbar = document.querySelectorAll(".show-navbar")
+
+leftAngle.forEach((btn)=> {
+    btn.addEventListener("click", function(){
+        margin-=100;
+        if(margin < 0){
+            margin = 200;
+        }
+        wrapper.style.marginLeft = "-" + margin + "vw";
+    })
+})
+
+rightAngle.forEach((btn)=> {
+    btn.addEventListener("click", function(){
+        margin+=100;
+        if(margin > 200){
+            margin = 0;
+        }
+        wrapper.style.marginLeft = "-" + margin + "vw";
+    })
+})
+
+showNavbar.forEach((div)=>{
+    div.addEventListener("click", function(){
+        document.getElementById("navbar-small-open").classList.toggle("margin-zero");
+        document.getElementById("opaque").classList.toggle("to-the-front")
+        document.body.classList.toggle("no-overflow")
+    })
+})
+console.log(screen.width, screen.height)
